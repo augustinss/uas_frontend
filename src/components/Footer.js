46071a0792b31ff931a62b3061a24ff2a381eb12
gigajdn <giga2100016020@webmail.uad.ac.id>
2023-07-25 12:03:08 +0700

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <h3>                                          </h3>
+        </div>
+    )
+}
+
+export default Footer;
